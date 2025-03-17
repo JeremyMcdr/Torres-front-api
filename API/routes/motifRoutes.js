2d132f509b827830ef3dcb2604e28e8a16dda6f1
JeremyMcdr <jeremymacadre@gmail.com>
2025-03-17 19:45:15 +0100

    ui

--- API/routes/motifRoutes.js
+++ API/routes/motifRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const motifController = require('../controllers/motifController');
 
-// Routes pour les données des motifs de commande
+// Routes pour les données des motifs de commandes
 router.get('/pourcentage', motifController.getPourcentageMotifsByAnnee);
 router.get('/list', motifController.getAvailableMotifs);
 
